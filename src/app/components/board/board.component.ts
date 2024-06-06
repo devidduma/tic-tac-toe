8d7bc22c4a8e31f54e48c604bc3320c7460a79dc
Devid Duma <devidd05@gmail.com>
2024-06-06 15:16:01 +0200

    feat(components): Board check winner

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -34,12 +34,54 @@ export class BoardComponent {
   }
 
   play(row: number, col: number) {
-    if(this.turn <= 9) {
+    if(this.turn == 0)
+      this.statusIndex = 1;
+
+    if(this.turn <= 9 && this.statusIndex == 1) {
+
       if((this.turn + this.turnShift) % 2 == 0) {
         this.noughtsPlay(row, col);
       } else {
         this.crossPlay(row, col);
       }
+
+      this.checkGameStatus();
+    }
+  }
+
+  checkGameStatus() {
+
+    for(let i: number = 0; i < 3; i++) {
+
+      // break
+      if(this.statusIndex != 1)
+        break;
+
+      // check winner
+      let productRows: number = 1;
+      let productCols: number = 1;
+      for(let ii: number = 0; ii < 3; ii++) {
+
+        productRows = productRows * this.board[i][ii];
+        productCols = productCols * this.board[ii][i];
+      }
+
+      this.checkWinner(productRows);
+      this.checkWinner(productCols);
+    }
+
+    let productDiag1: number = this.board[0][0] * this.board[1][1] * this.board[2][2];
+    let productDiag2: number = this.board[0][2] * this.board[1][1] * this.board[2][0];
+
+    this.checkWinner(productDiag1);
+    this.checkWinner(productDiag2);
+  }
+
+  checkWinner(product: number) {
+    if(product == 1) {
+      this.statusIndex = 3;
+    } else if(product == 8) {
+      this.statusIndex = 4;
     }
   }
 
