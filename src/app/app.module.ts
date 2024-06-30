ef55290b7803cd781e00ec205d8c388bdcd19c46
Devid Duma <devidd05@gmail.com>
2024-06-30 18:27:41 +0200

    build: Install Tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ScoresService} from "./services/scores.service";
 import {BoardComponent} from "./components/board/board.component";
 import {FeedbackComponent} from "./components/feedback/feedback.component";
 import {ScoresComponent} from "./components/scores/scores.component";
-import {ScoresService} from "./services/scores.service";
-import {FeedbackService} from "./services/feedback.service";
+
+
 
 @NgModule({
   declarations: [],
@@ -15,8 +16,7 @@ import {FeedbackService} from "./services/feedback.service";
     ScoresComponent
   ],
   providers: [
-    ScoresService,
-    FeedbackService
+    ScoresService
   ]
 })
 export class AppModule { }
