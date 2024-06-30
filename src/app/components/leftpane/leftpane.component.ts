f394b20a275ac1509e40fca5408d8181a04ddd41
Devid Duma <devidd05@gmail.com>
2024-06-30 18:27:41 +0200

    feat(Tailwind): Installed Tailwind CSS

---
+++ src/app/components/leftpane/leftpane.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leftpane',
+  standalone: true,
+  imports: [],
+  templateUrl: './leftpane.component.html',
+  styleUrl: './leftpane.component.css'
+})
+export class LeftpaneComponent {
+
+}
