89f4571510fd43eaeeac6b5955d446de65bd2ea8
Devid Duma <devidd05@gmail.com>
2025-02-16 14:10:05 +0100

    style(components): Left Pane padding and onClick

--- src/app/components/leftpane/leftpane.component.ts
+++ src/app/components/leftpane/leftpane.component.ts
@@ -2,11 +2,14 @@ import {Component, ViewChild} from '@angular/core';
 import {BoardComponent} from "../board/board.component";
 import {BoardService} from "../../services/board.service";
 import {FeedbackService} from "../../services/feedback.service";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-leftpane',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './leftpane.component.html',
   styleUrl: './leftpane.component.css'
 })
@@ -15,14 +18,18 @@ export class LeftpaneComponent {
   constructor(private boardService: BoardService, private feedbackService: FeedbackService) {
   }
 
+  public startGameWith: number = 0;
+
   firstToPlayNoughts() {
     this.boardService.firstToPlayNoughts();
+    this.startGameWith = 0;
     if(this.boardService.statusIndex == 0)
       this.feedbackService.setFeedbackMessage("Noughts starts first.");
   }
 
   firstToPlayCross() {
     this.boardService.firstToPlayCross();
+    this.startGameWith = 1;
     if(this.boardService.statusIndex == 0)
       this.feedbackService.setFeedbackMessage("Cross starts first.");
   }
