bd34bca3a6be0b00774da4039f01d8d38d30d791
Devid Duma <devidd05@gmail.com>
2025-07-23 10:51:57 +0200

    refactor: Feedback reactive

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,25 +1,25 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FeedbackService} from "../../services/feedback.service";
+import {Observable} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.css'
 })
-export class FeedbackComponent {
+export class FeedbackComponent implements OnInit {
 
-  feedbackMessage: string = "";
+  feedbackMessage!: Observable<string>;
 
   constructor(private feedbackService: FeedbackService) {
   }
 
   ngOnInit() {
-    this.feedbackService.getFeedbackMessage().subscribe(data => {
-      this.feedbackMessage = data
-    });
+    this.feedbackMessage = this.feedbackService.getFeedbackMessage();
   }
-
-  protected readonly console = console;
 }
