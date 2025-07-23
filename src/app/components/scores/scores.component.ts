4933622687f1a57ac0a14a5fc53035675b09a2c3
Devid Duma <devidd05@gmail.com>
2025-07-23 11:12:07 +0200

    refactor: Scores reactive

--- src/app/components/scores/scores.component.ts
+++ src/app/components/scores/scores.component.ts
@@ -2,33 +2,25 @@ import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {ScoresService} from "../../services/scores.service";
 import {AsyncPipe} from "@angular/common";
-import {FeedbackComponent} from "../feedback/feedback.component";
+import {Scores} from "../../commons/scores";
 
 @Component({
   selector: 'app-scores',
   standalone: true,
   imports: [
-    AsyncPipe,
-    FeedbackComponent
+    AsyncPipe
   ],
   templateUrl: './scores.component.html',
   styleUrl: './scores.component.css',
 })
 export class ScoresComponent implements OnInit {
-  protected pointsNoughts: number = 0;
-  protected pointsCross: number = 0;
+
+  scores!: Observable<Scores>;
 
   constructor(private scoresService: ScoresService) {
   }
 
   ngOnInit() {
-    this.scoresService.getScores().subscribe(data => {
-      this.pointsNoughts = data.pointsNoughts;
-      this.pointsCross = data.pointsCross;
-    });
-  }
-
-  showData() {
-    this.scoresService.showScores();
+    this.scores = this.scoresService.getScores();
   }
 }
