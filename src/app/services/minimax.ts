b8919b8b4ffa14bb35af380f0832817b940aeb61
Devid Duma <devidd05@gmail.com>
2025-07-27 15:58:50 +0200

    refactor(services): Minimax use Board

--- src/app/services/minimax.ts
+++ src/app/services/minimax.ts
@@ -1,4 +1,6 @@
 import {cloneDeep} from "lodash";
+import {Board} from "./board";
+import {GameStatus} from "../commons/game-status.enum";
 
 /*
  * Minimax algorithm
@@ -7,7 +9,7 @@ import {cloneDeep} from "lodash";
  */
 
 export class Minimax {
-  minimax(state: number[][], depth: number, player: number) {
+  minimax(board: Board, depth: number, player: number) {
     // Best
     let best: number[];
     if(player == 2) {
@@ -17,8 +19,8 @@ export class Minimax {
     }
 
     // Terminal state
-    if(depth == 0 || this.gameOver(state)) {
-      const score = this.evaluate(state);
+    if(board.statusIndex > GameStatus.Playing) {
+      const score = this.evaluate(board.statusIndex);
       return [-1, -1, score];
     }
 
@@ -26,14 +28,14 @@ export class Minimax {
     for(let r: number = 0; r < 3; r++) {
       for(let c: number = 0; c < 3; c++) {
         // Empty cells
-        if(state[r][c] != 0) {
+        if(board.state[r][c] != 0) {
           continue;
         }
 
         // Evaluate move for player
-        state[r][c] = player;
-        let score: number[] = this.minimax(cloneDeep(state), depth - 1, player % 2 + 1);
-        state[r][c] = 0;
+        const nextBoard = cloneDeep(board);
+        nextBoard.makeMove(r, c, player);
+        let score: number[] = this.minimax(nextBoard, depth - 1, player % 2 + 1);
         score[0] = r;
         score[1] = c;
 
@@ -53,42 +55,7 @@ export class Minimax {
     return best;
   }
 
-  private gameOver(state: number[][]) {
-    return this.checkWinner(state) != 0;
-  }
-
-  private evaluate(state: number[][]) {
-    const winner = this.checkWinner(state);
-    return winner == 2 ? 1 : winner == 1 ? -1 : 0;
-  }
-
-  private checkWinner(state: number[][]): number {
-    // Rows and Columns
-    for(let i = 0; i < 3; i++) {
-      let productRows: number = 1;
-      let productCols: number = 1;
-      for(let ii = 0; ii < 3; ii++) {
-        productRows = productRows * state[i][ii];
-        productCols = productCols * state[ii][i];
-      }
-      // Check winner
-      if(productRows == 1 || productCols == 1) {
-        return 1;
-      } else if(productRows == 8 || productCols == 8) {
-        return 2;
-      }
-    }
-    // Diagonals
-    let productDiag1: number = state[0][0] * state[1][1] * state[2][2];
-    let productDiag2: number = state[0][2] * state[1][1] * state[2][0];
-    // Check winner
-    if(productDiag1 == 1 || productDiag2 == 1) {
-      return 1;
-    } else if(productDiag1 == 8 || productDiag2 == 8) {
-      return 2;
-    }
-
-    // No winner
-    return 0;
+  private evaluate(statusIndex: GameStatus) {
+    return statusIndex == GameStatus.RobotWin ? 1 : statusIndex == GameStatus.HumanWin ? -1 : 0;
   }
 }
